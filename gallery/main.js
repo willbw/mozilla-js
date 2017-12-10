fc1e16e1302e04faa98c375c3f2daa219722f26d
Will <willwright90@gmail.com>
2017-12-10 20:25:01 +1100

    completed image gallery

--- gallery/main.js
+++ gallery/main.js
@@ -10,7 +10,22 @@ for (var i = 1; i < 6; i ++){
   var newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    displayedImage.setAttribute('src', e.target.getAttribute('src'));
+  }
 }
 
-/* Wiring up the Darken/Lighten button */
 
+/* Wiring up the Darken/Lighten button */
+btn.onclick = function(e) {
+	var currentClass = e.target.getAttribute('class');
+	if (currentClass == 'dark') {
+		e.target.setAttribute('class', 'light'); 
+		e.target.textContent = 'Lighten';
+		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+	} else {
+		e.target.setAttribute('class', 'dark'); 
+		e.target.textContent = 'Darken';
+		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+	}
+}
