4bd501fb45b1c5f9dcc65f18cc4d44ef4990c46c
Will <willwright90@gmail.com>
2017-12-11 21:15:19 +1100

    work on bouncing balls

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -12,3 +12,36 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function(){
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+Ball.prototype.update = function(){
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+  if ((this.y - this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+  this.x += this.velX;
+  this.y += this.velY;
+}
