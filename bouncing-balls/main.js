5921d840693425fad152bbc5e1b39f63c9291472
Will <willwright90@gmail.com>
2017-12-11 21:31:46 +1100

    stage one complete

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -45,3 +45,31 @@ Ball.prototype.update = function(){
   this.x += this.velX;
   this.y += this.velY;
 }
+
+var balls = [];
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 25) {
+    var ball = new Ball(
+      random(0,width),
+      random(0,height),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      random(10,20)
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
